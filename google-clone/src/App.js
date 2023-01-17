00615d0a50175a26e083a1222c0765718486bd83
Jyoti <jkgupta273@gmail.com>
2023-01-17 16:32:38 +0530

    Google-Clone

---
+++ google-clone/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom'; 
+
+// import Switch from "react-switch";
+import './App.css'
+import Home from "./components/Home"
+import Search from "./components/Search"
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/search" exact component={Search} />
+      </Switch>
+    </Router>
+  )
+}
+
+export default App;
